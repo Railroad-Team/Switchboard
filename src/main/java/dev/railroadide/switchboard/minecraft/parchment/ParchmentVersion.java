dd43cff7ceffac66b8391def6cb03d7216001f91
TurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2025-10-05 16:46:28 +0100

    Fix issue with parchment stuff not being committed

---
+++ src/main/java/dev/railroadide/switchboard/minecraft/parchment/ParchmentVersion.java
@@ -0,0 +1,15 @@
+package dev.railroadide.switchboard.minecraft.parchment;
+
+import java.time.LocalDate;
+
+public record ParchmentVersion(String version, String minecraftVersion,
+                               boolean isStable) implements Comparable<ParchmentVersion> {
+    @Override
+    public int compareTo(ParchmentVersion other) {
+        return parseVersion().compareTo(other.parseVersion());
+    }
+
+    private LocalDate parseVersion() {
+        return LocalDate.parse(version.replace(".", "-"));
+    }
+}
